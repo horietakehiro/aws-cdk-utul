aad0b18cdb67f046b9dd8c375ba66e4e5ddb35c6
horietakehiro <horie.takehiro@outlook.jp>
2024-10-29 18:37:42 +0900

    publish stack-manager module

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export * as cfn_resource_types from"./lib/types/cfn-resource-types"
 export * as typed_resource from "./lib/typed-resource"
 export * as assertions from "./lib/assertions"
+export * as stack_manager from "./lib/stack-manager"
