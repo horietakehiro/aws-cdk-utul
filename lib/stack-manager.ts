db779e7bbec6e92fd222c1a1c4e4bfead0d90133
horietakehiro <horie.takehiro@outlook.jp>
2024-11-12 16:05:27 +0900

    suppress stack manager module

--- lib/stack-manager.ts
+++ lib/stack-manager.ts
@@ -1,193 +1,193 @@
-import {
-  CloudFormationClient,
-  CloudFormationServiceException,
-  CreateStackCommand,
-  CreateStackCommandInput,
-  DeleteStackCommand,
-  DeleteStackCommandInput,
-  DescribeStacksCommand,
-  UpdateStackCommand,
-  UpdateStackCommandInput,
-  waitUntilStackCreateComplete,
-  waitUntilStackDeleteComplete,
-  waitUntilStackUpdateComplete,
-  Stack as cfnStack,
-} from "@aws-sdk/client-cloudformation";
-import { Stack as cdkStack } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
+// import {
+//   CloudFormationClient,
+//   CloudFormationServiceException,
+//   CreateStackCommand,
+//   CreateStackCommandInput,
+//   DeleteStackCommand,
+//   DeleteStackCommandInput,
+//   DescribeStacksCommand,
+//   UpdateStackCommand,
+//   UpdateStackCommandInput,
+//   waitUntilStackCreateComplete,
+//   waitUntilStackDeleteComplete,
+//   waitUntilStackUpdateComplete,
+//   Stack as cfnStack,
+// } from "@aws-sdk/client-cloudformation";
+// import { Stack as cdkStack } from "aws-cdk-lib";
+// import { Template } from "aws-cdk-lib/assertions";
 
-type DeployStackInput = Omit<
-  CreateStackCommandInput | UpdateStackCommandInput,
-  "StackName"
->;
-type DestroyStackInput = Omit<DeleteStackCommandInput, "StackName">;
+// type DeployStackInput = Omit<
+//   CreateStackCommandInput | UpdateStackCommandInput,
+//   "StackName"
+// >;
+// type DestroyStackInput = Omit<DeleteStackCommandInput, "StackName">;
 
-/**
- * Stack Manager makes it easier and more efficient that you deploy(and destroy) stacks with short-term-lifecycle duraing tests.
- */
-export class StackManager {
-  readonly stack: cdkStack;
-  readonly client: CloudFormationClient;
+// /**
+//  * Stack Manager makes it easier and more efficient that you deploy(and destroy) stacks with short-term-lifecycle duraing tests.
+//  */
+// export class StackManager {
+//   readonly stack: cdkStack;
+//   readonly client: CloudFormationClient;
 
-  /**
-   *
-   * @param stack stack to be managed
-   * @param client cloudformation sdk client to use
-   */
-  constructor(stack: cdkStack, client: CloudFormationClient) {
-    this.stack = stack;
-    this.client = client;
-  }
+//   /**
+//    *
+//    * @param stack stack to be managed
+//    * @param client cloudformation sdk client to use
+//    */
+//   constructor(stack: cdkStack, client: CloudFormationClient) {
+//     this.stack = stack;
+//     this.client = client;
+//   }
 
-  /**
-   * get specific output value from the stack
-   * @param outputKey logical id of Output that you want to get
-   * @returns
-   */
-  async getStackOutput(outputKey: string): Promise<string> {
-    console.debug(`get outputs from stack ${this.stack.stackName}`);
-    const describeStacksResponse = await this.client.send(
-      new DescribeStacksCommand({
-        StackName: this.stack.stackName,
-      })
-    );
-    const stacks = describeStacksResponse.Stacks ?? [];
-    if (stacks.length === 0) {
-      throw Error(
-        `failed to get output value with key ${outputKey} from stack ${this.stack}`
-      );
-    }
-    const outputs = stacks[0].Outputs ?? [];
-    console.debug(
-      `outputs of stack ${this.stack.stackName} are : ${JSON.stringify(outputs)}`
-    );
-    const output = outputs.filter((o) => o.OutputKey === outputKey);
-    if (output.length === 0 || output[0].OutputValue === undefined) {
-      throw Error(
-        `failed to get output value with key ${outputKey} from stack ${this.stack}`
-      );
-    }
-    return output[0].OutputValue;
-  }
+//   /**
+//    * get specific output value from the stack
+//    * @param outputKey logical id of Output that you want to get
+//    * @returns
+//    */
+//   async getStackOutput(outputKey: string): Promise<string> {
+//     console.debug(`get outputs from stack ${this.stack.stackName}`);
+//     const describeStacksResponse = await this.client.send(
+//       new DescribeStacksCommand({
+//         StackName: this.stack.stackName,
+//       })
+//     );
+//     const stacks = describeStacksResponse.Stacks ?? [];
+//     if (stacks.length === 0) {
+//       throw Error(
+//         `failed to get output value with key ${outputKey} from stack ${this.stack}`
+//       );
+//     }
+//     const outputs = stacks[0].Outputs ?? [];
+//     console.debug(
+//       `outputs of stack ${this.stack.stackName} are : ${JSON.stringify(outputs)}`
+//     );
+//     const output = outputs.filter((o) => o.OutputKey === outputKey);
+//     if (output.length === 0 || output[0].OutputValue === undefined) {
+//       throw Error(
+//         `failed to get output value with key ${outputKey} from stack ${this.stack}`
+//       );
+//     }
+//     return output[0].OutputValue;
+//   }
 
-  /**
-   * Deploy the specified stack.
-   *
-   * If the specified stack has not been created yet, create new one. If already created, update it.
-   *
-   * This method waits until stack deployment gets completed.
-   * @param deployStackInput
-   * @param maxWaitTimeInSeconds the default values is `60 * 60 = 1hour`. Allowed minimum value is `31 seconds`
-   */
-  async deployStack(
-    deployStackInput: DeployStackInput,
-    maxWaitTimeInSeconds?: number
-  ) {
-    if (maxWaitTimeInSeconds === undefined) {
-      console.debug("set 1hour as maxWaitTimeInSeconds default value");
-      maxWaitTimeInSeconds = 60 * 60;
-    }
-    if (maxWaitTimeInSeconds <= 30) {
-      throw  Error("maxWaitTimeInSeconds must be greater than 30")
-    }
-    console.debug(`check if stack ${this.stack.stackName} exists`);
-    let existStacks: cfnStack[] = [];
-    try {
-      const describeStaacksResponse = await this.client.send(
-        new DescribeStacksCommand({
-          StackName: this.stack.stackName,
-        })
-      );
-      existStacks = describeStaacksResponse.Stacks ?? [];
-    } catch (error) {
-      if (
-        !(
-          error instanceof CloudFormationServiceException &&
-          error.message.includes("does not exist")
-        )
-      ) {
-        throw error;
-      }
-    }
-    if (existStacks.length === 0) {
-      console.debug(`stack ${this.stack.stackName} does not exist, create new`);
-      await this.client.send(
-        new CreateStackCommand({
-          ...deployStackInput,
-          StackName: this.stack.stackName,
-          TemplateBody: JSON.stringify(Template.fromStack(this.stack)),
-        })
-      );
-      console.debug(
-        `wait until deploying of stack ${this.stack.stackName} completes`
-      );
-      await waitUntilStackCreateComplete(
-        { client: this.client, maxWaitTime: maxWaitTimeInSeconds },
-        { StackName: this.stack.stackName }
-      );
-    } else {
-      try {
-        console.debug(`stack ${this.stack.stackName} exists, update it`);
-        await this.client.send(
-          new UpdateStackCommand({
-            ...deployStackInput,
-            StackName: this.stack.stackName,
-            TemplateBody: JSON.stringify(Template.fromStack(this.stack)),
-          })
-        );
-        console.debug(
-          `wait until deploying of stack ${this.stack.stackName} completes`
-        );
-        await waitUntilStackUpdateComplete(
-          { client: this.client, maxWaitTime: maxWaitTimeInSeconds },
-          { StackName: this.stack.stackName }
-        );
-      } catch (error) {
-        if (
-          error instanceof CloudFormationServiceException &&
-          error.message === "No updates are to be performed."
-        ) {
-          console.debug("no updates are to be performed");
-        } else {
-          throw error;
-        }
-      }
-    }
-  }
-  /**
-   * Destroy the stack
-   * @param destroyStackInput
-   * @param wait If `true`, this method waits until stack deletion complete. The default value is 'false'
-   * @param maxWaitTimeInSeconds the default value is `60 * 60 = 1hour`. Allowed minimum value is `31 seconds`. This param will be used only when parameter `wait` is set to 'true'
-   */
-  async destroyStack(
-    destroyStackInput: DestroyStackInput,
-    wait?: boolean,
-    maxWaitTimeInSeconds?: number
-  ): Promise<void> {
-    console.debug(`destroy stack ${this.stack.stackName}`);
-    await this.client.send(
-      new DeleteStackCommand({
-        StackName: this.stack.stackName,
-        ...destroyStackInput,
-      })
-    );
-    if (!wait || wait === undefined) {
-      console.debug("return asynchronously");
-      return;
-    }
+//   /**
+//    * Deploy the specified stack.
+//    *
+//    * If the specified stack has not been created yet, create new one. If already created, update it.
+//    *
+//    * This method waits until stack deployment gets completed.
+//    * @param deployStackInput
+//    * @param maxWaitTimeInSeconds the default values is `60 * 60 = 1hour`. Allowed minimum value is `31 seconds`
+//    */
+//   async deployStack(
+//     deployStackInput: DeployStackInput,
+//     maxWaitTimeInSeconds?: number
+//   ) {
+//     if (maxWaitTimeInSeconds === undefined) {
+//       console.debug("set 1hour as maxWaitTimeInSeconds default value");
+//       maxWaitTimeInSeconds = 60 * 60;
+//     }
+//     if (maxWaitTimeInSeconds <= 30) {
+//       throw  Error("maxWaitTimeInSeconds must be greater than 30")
+//     }
+//     console.debug(`check if stack ${this.stack.stackName} exists`);
+//     let existStacks: cfnStack[] = [];
+//     try {
+//       const describeStaacksResponse = await this.client.send(
+//         new DescribeStacksCommand({
+//           StackName: this.stack.stackName,
+//         })
+//       );
+//       existStacks = describeStaacksResponse.Stacks ?? [];
+//     } catch (error) {
+//       if (
+//         !(
+//           error instanceof CloudFormationServiceException &&
+//           error.message.includes("does not exist")
+//         )
+//       ) {
+//         throw error;
+//       }
+//     }
+//     if (existStacks.length === 0) {
+//       console.debug(`stack ${this.stack.stackName} does not exist, create new`);
+//       await this.client.send(
+//         new CreateStackCommand({
+//           ...deployStackInput,
+//           StackName: this.stack.stackName,
+//           TemplateBody: JSON.stringify(Template.fromStack(this.stack)),
+//         })
+//       );
+//       console.debug(
+//         `wait until deploying of stack ${this.stack.stackName} completes`
+//       );
+//       await waitUntilStackCreateComplete(
+//         { client: this.client, maxWaitTime: maxWaitTimeInSeconds },
+//         { StackName: this.stack.stackName }
+//       );
+//     } else {
+//       try {
+//         console.debug(`stack ${this.stack.stackName} exists, update it`);
+//         await this.client.send(
+//           new UpdateStackCommand({
+//             ...deployStackInput,
+//             StackName: this.stack.stackName,
+//             TemplateBody: JSON.stringify(Template.fromStack(this.stack)),
+//           })
+//         );
+//         console.debug(
+//           `wait until deploying of stack ${this.stack.stackName} completes`
+//         );
+//         await waitUntilStackUpdateComplete(
+//           { client: this.client, maxWaitTime: maxWaitTimeInSeconds },
+//           { StackName: this.stack.stackName }
+//         );
+//       } catch (error) {
+//         if (
+//           error instanceof CloudFormationServiceException &&
+//           error.message === "No updates are to be performed."
+//         ) {
+//           console.debug("no updates are to be performed");
+//         } else {
+//           throw error;
+//         }
+//       }
+//     }
+//   }
+//   /**
+//    * Destroy the stack
+//    * @param destroyStackInput
+//    * @param wait If `true`, this method waits until stack deletion complete. The default value is 'false'
+//    * @param maxWaitTimeInSeconds the default value is `60 * 60 = 1hour`. Allowed minimum value is `31 seconds`. This param will be used only when parameter `wait` is set to 'true'
+//    */
+//   async destroyStack(
+//     destroyStackInput: DestroyStackInput,
+//     wait?: boolean,
+//     maxWaitTimeInSeconds?: number
+//   ): Promise<void> {
+//     console.debug(`destroy stack ${this.stack.stackName}`);
+//     await this.client.send(
+//       new DeleteStackCommand({
+//         StackName: this.stack.stackName,
+//         ...destroyStackInput,
+//       })
+//     );
+//     if (!wait || wait === undefined) {
+//       console.debug("return asynchronously");
+//       return;
+//     }
 
-    console.debug("wait until stack deletion complets");
-    if (maxWaitTimeInSeconds === undefined) {
-      maxWaitTimeInSeconds = 60 * 60;
-    }
-    if (maxWaitTimeInSeconds <= 30) {
-      throw  Error("maxWaitTimeInSeconds must be greater than 30")
-    }
-    await waitUntilStackDeleteComplete(
-      { client: this.client, maxWaitTime: maxWaitTimeInSeconds },
-      { StackName: this.stack.stackName }
-    );
-    return;
-  }
-}
+//     console.debug("wait until stack deletion complets");
+//     if (maxWaitTimeInSeconds === undefined) {
+//       maxWaitTimeInSeconds = 60 * 60;
+//     }
+//     if (maxWaitTimeInSeconds <= 30) {
+//       throw  Error("maxWaitTimeInSeconds must be greater than 30")
+//     }
+//     await waitUntilStackDeleteComplete(
+//       { client: this.client, maxWaitTime: maxWaitTimeInSeconds },
+//       { StackName: this.stack.stackName }
+//     );
+//     return;
+//   }
+// }
--- test/unit/stack-manager.test.ts
+++ test/unit/stack-manager.test.ts
@@ -1,168 +1,169 @@
-import { v4 as uuidv4 } from "uuid"
-import {NodeHttpHandler} from "@smithy/node-http-handler"
-import { HttpsProxyAgent } from "hpagent"
-import { 
-  CfnOutput,
-  RemovalPolicy,
-  Stack,
-  aws_s3 as s3,
-} from "aws-cdk-lib"
+// import { v4 as uuidv4 } from "uuid"
+// import {NodeHttpHandler} from "@smithy/node-http-handler"
+// import { HttpsProxyAgent } from "hpagent"
+// import { 
+//   CfnOutput,
+//   RemovalPolicy,
+//   Stack,
+//   aws_s3 as s3,
+// } from "aws-cdk-lib"
 
-import {
-  StackManager
-} from "../../lib/stack-manager"
-import { CloudFormationClient, DeleteStackCommand, DescribeStacksCommand, StackStatus } from "@aws-sdk/client-cloudformation"
+// import {
+//   StackManager
+// } from "../../lib/stack-manager"
+// import { CloudFormationClient, DeleteStackCommand, DescribeStacksCommand, StackStatus } from "@aws-sdk/client-cloudformation"
 
-const TIMEOUT = 10 * 60 * 1000
+// const TIMEOUT = 10 * 60 * 1000
 
-const buildRequestHandler = ():NodeHttpHandler => {
-    const httpProxyEnvVar = process.env.http_proxy
-    const proxyAgent = httpProxyEnvVar ? new HttpsProxyAgent({proxy: httpProxyEnvVar}) : undefined
-    return new NodeHttpHandler({
-        httpAgent: proxyAgent,
-        httpsAgent: proxyAgent,
-    })
-}
-const client = new CloudFormationClient({
-  requestHandler: buildRequestHandler()
-})
+// const buildRequestHandler = ():NodeHttpHandler => {
+//     const httpProxyEnvVar = process.env.http_proxy
+//     const proxyAgent = httpProxyEnvVar ? new HttpsProxyAgent({proxy: httpProxyEnvVar}) : undefined
+//     return new NodeHttpHandler({
+//         httpAgent: proxyAgent,
+//         httpsAgent: proxyAgent,
+//     })
+// }
+// const client = new CloudFormationClient({
+//   requestHandler: buildRequestHandler()
+// })
 
-const uniqueStackName = () => `test-stack-${uuidv4()}`
-const removalBucket = (stack:Stack, id:string) => {
-  const b = new s3.Bucket(stack, id, {})
-  b.applyRemovalPolicy(RemovalPolicy.DESTROY)
-  return b
-}
+// const uniqueStackName = () => `test-stack-${uuidv4()}`
+// const removalBucket = (stack:Stack, id:string) => {
+//   const b = new s3.Bucket(stack, id, {})
+//   b.applyRemovalPolicy(RemovalPolicy.DESTROY)
+//   return b
+// }
 
-describe("StackManager", () => {
-  test("deployStack create a new stack if not created yet", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")
+// describe("StackManager", () => {
+//   test("deployStack create a new stack if not created yet", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")
     
-    const stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
+//     const stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
 
-    const describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    const stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//     const describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     const stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    await client.send(new DeleteStackCommand({StackName: stack.stackName}))
+//     await client.send(new DeleteStackCommand({StackName: stack.stackName}))
 
-  }, TIMEOUT)
+//   }, TIMEOUT)
 
-  test("deployStack can customize parameters", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")
+//   test("deployStack can customize parameters", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")
     
-    const stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({
-      Tags: [{Key: "hoge", Value: "fuga"}]
-    }, 45)
+//     const stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({
+//       Tags: [{Key: "hoge", Value: "fuga"}]
+//     }, 45)
 
-    const describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    const stacks = describeStacksResponse.Stacks ?? []
-    const tags = stacks[0].Tags ?? []
-    expect(tags[0].Key).toBe("hoge")
-    expect(tags[0].Value).toBe("fuga")
+//     const describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     const stacks = describeStacksResponse.Stacks ?? []
+//     const tags = stacks[0].Tags ?? []
+//     expect(tags[0].Key).toBe("hoge")
+//     expect(tags[0].Value).toBe("fuga")
 
-    await client.send(new DeleteStackCommand({StackName: stack.stackName}))
+//     await client.send(new DeleteStackCommand({StackName: stack.stackName}))
 
-  }, TIMEOUT)
+//   }, TIMEOUT)
 
-  test("deployStack raise error before deployment when maxWaitTimeSeconds is not greater than 30", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")
+//   test("deployStack raise error before deployment when maxWaitTimeSeconds is not greater than 30", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")
     
-    const stackManager = new StackManager(stack, client)
-    expect(stackManager.deployStack({}, 30)).rejects.toThrow("maxWaitTimeInSeconds must be greater than 30")
+//     const stackManager = new StackManager(stack, client)
+//     expect(stackManager.deployStack({}, 30)).rejects.toThrow("maxWaitTimeInSeconds must be greater than 30")
 
-  }, TIMEOUT)
+//   }, TIMEOUT)
 
-  test("deployStack update a stack if created already", async () => {
-    const stackName = uniqueStackName()
-    let stack = new Stack(undefined, stackName)
-    removalBucket(stack, "bucket1")
+//   test("deployStack update a stack if created already", async () => {
+//     const stackName = uniqueStackName()
+//     let stack = new Stack(undefined, stackName)
+//     removalBucket(stack, "bucket1")
 
-    let stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
-    let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    let stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//     let stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
+//     let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     let stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    stack = new Stack(undefined, stackName)
-    removalBucket(stack, "bucket2")
-    stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
-    describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.UPDATE_COMPLETE)
+//     stack = new Stack(undefined, stackName)
+//     removalBucket(stack, "bucket2")
+//     stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
+//     describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.UPDATE_COMPLETE)
 
-    await client.send(new DeleteStackCommand({StackName: stack.stackName}))
+//     await client.send(new DeleteStackCommand({StackName: stack.stackName}))
 
-  }, TIMEOUT)
+//   }, TIMEOUT)
 
-  test("deployStack does not raise error if no update to be applied to the stack", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")    
-    const stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
-    let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    let stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//   test("deployStack does not raise error if no update to be applied to the stack", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")    
+//     const stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
+//     let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     let stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    await stackManager.deployStack({})
-    describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//     await stackManager.deployStack({})
+//     describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    await client.send(new DeleteStackCommand({StackName: stack.stackName}))
-  }, TIMEOUT)
+//     await client.send(new DeleteStackCommand({StackName: stack.stackName}))
+//   }, TIMEOUT)
 
- test("destroyStack delete stack asyncronoucely with default config", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")    
-    const stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
-    let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    let stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//  test("destroyStack delete stack asyncronoucely with default config", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")    
+//     const stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
+//     let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     let stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    const start = Date.now()
-    await stackManager.destroyStack({})
-    const end = Date.now()
+//     const start = Date.now()
+//     await stackManager.destroyStack({})
+//     const end = Date.now()
 
-    expect(end - start).toBeLessThan(3 * 1000)
- }, TIMEOUT)
- test("destroyStack delete stack syncronoucely", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")    
-    const stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
-    let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    let stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//     expect(end - start).toBeLessThan(3 * 1000)
+//  }, TIMEOUT)
+//  test("destroyStack delete stack syncronoucely", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")    
+//     const stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
+//     let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     let stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    const start = Date.now()
-    await stackManager.destroyStack({}, true)
-    const end = Date.now()
+//     const start = Date.now()
+//     await stackManager.destroyStack({}, true)
+//     const end = Date.now()
 
-    expect(end - start).toBeGreaterThan(5 * 1000)
- }, TIMEOUT)
+//     expect(end - start).toBeGreaterThan(5 * 1000)
+//  }, TIMEOUT)
 
- test("getOutput get output value of stack", async () => {
-    const stack = new Stack(undefined, uniqueStackName())
-    removalBucket(stack, "bucket")    
-    new CfnOutput(stack, "Output1", {value: "value1"})
-    new CfnOutput(stack, "Output2", {value: "value2"})
-    const stackManager = new StackManager(stack, client)
-    await stackManager.deployStack({})
-    let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
-    let stacks = describeStacksResponse.Stacks ?? []
-    expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
+//  test("getOutput get output value of stack", async () => {
+//     const stack = new Stack(undefined, uniqueStackName())
+//     removalBucket(stack, "bucket")    
+//     new CfnOutput(stack, "Output1", {value: "value1"})
+//     new CfnOutput(stack, "Output2", {value: "value2"})
+//     const stackManager = new StackManager(stack, client)
+//     await stackManager.deployStack({})
+//     let describeStacksResponse = await client.send(new DescribeStacksCommand({StackName: stack.stackName}))
+//     let stacks = describeStacksResponse.Stacks ?? []
+//     expect(stacks[0].StackStatus).toBe(StackStatus.CREATE_COMPLETE)
 
-    expect(await stackManager.getStackOutput("Output1")).toBe("value1")
-    expect(await stackManager.getStackOutput("Output2")).toBe("value2")
+//     expect(await stackManager.getStackOutput("Output1")).toBe("value1")
+//     expect(await stackManager.getStackOutput("Output2")).toBe("value2")
 
-    await stackManager.destroyStack({}, true)
- }, TIMEOUT)
-})
+//     await stackManager.destroyStack({}, true)
+//  }, TIMEOUT)
+// })
+test.skip("stack manager module has been depressed", () => {})
