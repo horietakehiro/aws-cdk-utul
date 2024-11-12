1dec7524c74b8678a0d9f0a3c22dc7c6e020c98c
horietakehiro <horie.takehiro@outlook.jp>
2024-11-12 16:07:07 +0900

    add  method

--- test/e2e/assertions.test.ts
+++ test/e2e/assertions.test.ts
@@ -1,100 +1,126 @@
-import { App } from "aws-cdk-lib"
-import { TestStack, TestStackProps } from "./test-stack"
-import { ExtraMatch, TypedTemplate } from "../../lib/assertions"
-import { Match, Template } from "aws-cdk-lib/assertions"
-import { AWS_EC2_VPC, AWS_EC2_SUBNET, AWS_EC2_INSTANCE, AWS_EC2_EIP, AWS_IAM_ROLE } from "./../../lib/types/cfn-resource-types"
+import { App } from "aws-cdk-lib";
+import { TestStack, TestStackProps } from "./test-stack";
+import { ExtraMatch, TypedTemplate } from "../../lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import {
+  AWS_EC2_VPC,
+  AWS_EC2_SUBNET,
+  AWS_EC2_INSTANCE,
+  AWS_EC2_EIP,
+  AWS_IAM_ROLE,
+} from "./../../lib/types/cfn-resource-types";
 
-const app = new App()
-const stackProps:TestStackProps = {cidr: "10.0.0.0/16"}
-const stack = new TestStack(app, "TestStack", stackProps)
-const template = new TypedTemplate(Template.fromStack(stack))
+const app = new App();
+const stackProps: TestStackProps = { cidr: "10.0.0.0/16" };
+const stack = new TestStack(app, "TestStack", stackProps);
+const template = TypedTemplate.fromStack(stack);
 describe("TypedTemplate", () => {
+  test.skip("fromStack - obviously passed", () => {});
   test("resourceCountIs", () => {
-    template.resourceCountIs(AWS_EC2_SUBNET({}), 2)
-  })
+    template.resourceCountIs(AWS_EC2_SUBNET(), 2);
+  });
   test("resourcePropertiesCountIs", () => {
-    template.resourcePropertiesCountIs(AWS_EC2_VPC({Properties: {CidrBlock: stackProps.cidr}}), 1)
-  })
+    template.resourcePropertiesCountIs(
+      AWS_EC2_VPC({ Properties: { CidrBlock: stackProps.cidr } }),
+      1
+    );
+  });
   test("hasResourceProperties", () => {
     template.hasResourceProperties(
-      AWS_EC2_INSTANCE({Properties: {
-        InstanceType: "t2.micro"
-      }})
-    )
-  })
+      AWS_EC2_INSTANCE({
+        Properties: {
+          InstanceType: "t2.micro",
+        },
+      })
+    );
+  });
   test("hasResource", () => {
-    template.hasResource(AWS_EC2_INSTANCE({}))
-  })
+    template.hasResource(AWS_EC2_INSTANCE({}));
+  });
 
   test("findResources", () => {
-    const subnets = template.findResources(AWS_EC2_SUBNET({}))
-    expect(subnets.length).toBe(2)
-  })
+    const subnets = template.findResources(AWS_EC2_SUBNET({}));
+    expect(subnets.length).toBe(2);
+  });
   test("allResources", () => {
-    template.allResources(AWS_EC2_SUBNET({Properties: {
-      MapPublicIpOnLaunch: true,
-    }}))
-  })
+    template.allResources(
+      AWS_EC2_SUBNET({
+        Properties: {
+          MapPublicIpOnLaunch: true,
+        },
+      })
+    );
+  });
   test("allResourcesProperties", () => {
-    template.allResourcesProperties(AWS_EC2_SUBNET({Properties: {
-      MapPublicIpOnLaunch: true
-    }}))
-  })
+    template.allResourcesProperties(
+      AWS_EC2_SUBNET({
+        Properties: {
+          MapPublicIpOnLaunch: true,
+        },
+      })
+    );
+  });
   test("hasParameter", () => {
     template.hasParameter("*", {
-      Type: "AWS::EC2::SecurityGroup::Id"
-    })
-  })
+      Type: "AWS::EC2::SecurityGroup::Id",
+    });
+  });
   test("findParameters", () => {
     const parameters = template.findParameters("*", {
-      Type: "AWS::EC2::SecurityGroup::Id"
-    })
-    expect(parameters.length).toBe(1)
-    expect(parameters[0].def.Description).toBe("security group id")
-  })
+      Type: "AWS::EC2::SecurityGroup::Id",
+    });
+    expect(parameters.length).toBe(1);
+    expect(parameters[0].def.Description).toBe("security group id");
+  });
   test("hasOutput", () => {
-    template.hasOutput("*", {})
-  })
+    template.hasOutput("*", {});
+  });
   test("findOutputs", () => {
     const outputs = template.findOutputs("*", {
-      Description: "role arn"
-    })
-    expect(outputs.length).toBe(1)
-  })
+      Description: "role arn",
+    });
+    expect(outputs.length).toBe(1);
+  });
   test("hasMapping", () => {
-    template.hasMapping("*", {})
-  })
+    template.hasMapping("*", {});
+  });
   test("findMappings", () => {
-    const mappings = template.findMappings("*", {})
-    expect(mappings.length).toBe(1)
-  })
+    const mappings = template.findMappings("*", {});
+    expect(mappings.length).toBe(1);
+  });
   test("hasCondition", () => {
-    template.hasCondition("*", {})
-  })
+    template.hasCondition("*", {});
+  });
   test("findConditions", () => {
-    const conditions = template.findConditions("*", {})
-    expect(conditions.length).toBe(1)
-  })
-})
+    const conditions = template.findConditions("*", {});
+    expect(conditions.length).toBe(1);
+  });
+});
 
 describe("ExtraMatch", () => {
   test("ref", () => {
-    const subnets = template.findResources(AWS_EC2_SUBNET({}))
-    template.hasResource(AWS_EC2_INSTANCE({Properties: {
-      SubnetId: ExtraMatch.ref(subnets[0].id)
-    }}))
-  })
+    const subnets = template.findResources(AWS_EC2_SUBNET({}));
+    template.hasResource(
+      AWS_EC2_INSTANCE({
+        Properties: {
+          SubnetId: ExtraMatch.ref(subnets[0].id),
+        },
+      })
+    );
+  });
   test("getAtt", () => {
-    const eip = template.findResources(AWS_EC2_EIP({}))
+    const eip = template.findResources(AWS_EC2_EIP({}));
     const outputs = template.findOutputs("*", {
-        Value: ExtraMatch.getAtt(eip[0].id, "PublicIp")
-    })
-    expect(outputs.length).toBe(1)
-  })
+      Value: ExtraMatch.getAtt(eip[0].id, "PublicIp"),
+    });
+    expect(outputs.length).toBe(1);
+  });
   test("getAttArn", () => {
-    const roles = template.findResources(AWS_IAM_ROLE({Properties: {Description: "role"}}))
+    const roles = template.findResources(
+      AWS_IAM_ROLE({ Properties: { Description: "role" } })
+    );
     template.hasOutput("*", {
-      Value: ExtraMatch.getAttArn(roles[0].id)
-    })
-  })
-})
+      Value: ExtraMatch.getAttArn(roles[0].id),
+    });
+  });
+});
